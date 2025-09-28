a983b6b63cd2209af1d7f025c7d265bb15bfa8cf
Wahyu Slamet Adi Triyono <wahyuslamet.adi@gmail.com>
2025-09-28 09:27:41 +0700

    Menambahkan variable

--- bin/latihan_dart_1.dart
+++ bin/latihan_dart_1.dart
@@ -1,4 +1,14 @@
 
 void main(List<String> arguments) {
   print('Hello world latihan dart 1');
+  var fullname = 'Dart Programing';
+  var age = 1;
+  var height = 1.79;
+  var IsMarried = false;
+
+  print(fullname);
+  print(age);
+  print(IsMarried);
+
+  
 }
